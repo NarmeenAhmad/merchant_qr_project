aed459f6273b587333dcbd95b6ded9b7df229c6d
NarmeenAhmad <narmeen9565@gmail.com>
2025-03-07 23:38:04 +0530

    Renamed project to Restaurant-Menu-QR-Code-Generator

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'merchant_QR.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restaurant-Menu-QR-Code-Generator.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
